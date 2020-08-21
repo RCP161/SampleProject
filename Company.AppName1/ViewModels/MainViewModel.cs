b95610b421f60dcde7b6f924759cab4d97f1473c
Patrick Rahner <patrick.rahner@inotech.eu>
2020-08-21 15:08:49 +0200

    Sicherung

--- Company.AppName1/ViewModels/MainViewModel.cs
+++ Company.AppName1/ViewModels/MainViewModel.cs
@@ -2,20 +2,25 @@
 {
     using Catel.Data;
     using Catel.MVVM;
+    using Catel.Services;
     using Company.AppName1.Models;
     using Company.Base.Core;
+    using Company.Base.Presentation;
+    using System;
     using System.Collections.ObjectModel;
     using System.Threading.Tasks;
 
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : InoViewModelBase1<MainModel>
     {
         public MainViewModel()
         {
-            Model = new MainModel();
+            throw new Exception("Alles auf .NetCore 3.0 Umabauen");
 
-            ActivCommand = new Command<InoModelBase1>(SetActivatedModule);
+            //Model = new MainModel();
+
+            //ActivCommand = new Command<InoModelBase1>(SetActivatedModule);
+
         }
-
 
         #region Properties
         public override string Title { get { return "Welcome to Company.AppName1"; } }
@@ -23,15 +28,6 @@
         // TODO: Register models with the vmpropmodel codesnippet
         // TODO: Register view model properties with the vmprop or vmpropviewmodeltomodel codesnippets
         // TODO: Register commands with the vmcommand or vmcommandwithcanexecute codesnippets
-
-
-        [Model]
-        public MainModel Model
-        {
-            get { return GetValue<MainModel>(ModelProperty); }
-            private set { SetValue(ModelProperty, value); }
-        }
-        public static readonly PropertyData ModelProperty = RegisterProperty(nameof(Model), typeof(MainModel));
 
 
         public Command<InoModelBase1> ActivCommand { get; private set; }
