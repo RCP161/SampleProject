2f3047e680d96d9de5eb2832b18276557d3f4fe7
Patrick Rahner <patrick.rahner@inotech.eu>
2020-08-31 10:37:05 +0200

    Sicherung

---
+++ Company.Styling/Controls/InoHamburgerMenu.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MahApps.Metro.Controls;
+
+namespace Company.Styling.Controls
+{
+    public class InoHamburgerMenu : HamburgerMenu
+    {
+    }
+}
